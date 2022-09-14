5a7be5c81d3b4c5fe0dd48b1bf002321c45e64b3
kallasrikanth1999 <kalla.srikanth010@gmail.com>
2022-09-14 12:54:01 +0530

    fuel
    fuelcon

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template,url_for,request,redirect
+import numpy as np
+import pandas as pd
+import joblib
+import pickle
+
+
+app = Flask(__name__)
+
+model = joblib.load('RFR.pkl')
+onehot = joblib.load('five_joblib')
+
+
+@app.route('/')
+@app.route('/main')
+def main():
+	return render_template('main.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	int_features =[[x for x in request.form.values()]]
+	c = ["make","model","vehicle_class","transmission","fuel","engine","cylinder","co2","co2Rating","smokerating"]
+	df = pd.DataFrame(int_features,columns=c)
+	l = onehot.transform(df.iloc[:,:5])
+	c = onehot.get_feature_names_out()
+	t = pd.DataFrame(l,columns=c)
+	l2 = df.iloc[:,5:]
+	final =pd.concat([l2,t],axis=1)
+	result = model.predict(final)
+	print("The Result is :",result)
+
+
+	print(int_features)
+
+	return render_template("main.html",prediction_text=" The Estimated Fuel consumption Rating is: {}".format(result))
+
+
+if __name__ == "__main__":
+	app.debug=True
+	app.run(host = '127.0.0.1', port =7000)
